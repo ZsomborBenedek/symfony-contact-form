be130cea903cc841e8dc8c7cb893a022072f37d0
Zsombor Benedek <zsomborbeni@gmail.com>
2021-06-08 20:36:27 +0200

    added labels to UserType form class

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,15 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('message')
-        ;
+            ->add('name', TextType::class, [
+                'label' => 'Neved'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail címed'
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Üzenet szövege'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
